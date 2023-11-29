1d0a231ffd4256bfecfbca52bb29b1290bc63571
Furkan İBİŞ <102908626+furkannibis@users.noreply.github.com>
2023-11-29 17:33:22 +0300

    Add files via upload

---
+++ DISTINC_COUNT.sql
@@ -0,0 +1,8 @@
+-- SELECT DISTINCT rental_rate FROM film;
+-- SELECT DISTINCT replacement_cost FROM film;
+-- SELECT DISTINCT replacement_cost, rental_rate FROM film;
+-- SELECT COUNT(*) FROM actor WHERE first_name = 'Penelope'; --> 4
+-- SELECT COUNT(*) FROM actor WHERE first_name LIKE 'A%'; --> 13
+-- SELECT COUNT(first_name) FROM actor WHERE first_name LIKE 'A%'; --> 13
+-- SELECT COUNT(DISTINCT first_name) FROM actor;
+-- SELECT COUNT(DISTINCT length) FROM film;
---
+++ homework4.sql
@@ -0,0 +1,5 @@
+-- SELECT DISTINCT replacement_cost FROM film;
+-- SELECT COUNT(DISTINCT replacement_cost) FROM film;
+-- SELECT COUNT(title) FROM film WHERE title LIKE 'T%' and rating = 'G';
+-- SELECT COUNT(*) FROM country WHERE LENGTH(country) = 5;
+-- SELECT city FROM city WHERE city ~~* '%R';
